63cec2192bf6b0d55818d1385d9357ba257d35aa
Steve Jrong <Steve Jrong@SteveJrong-PC>
2018-03-16 21:50:10 +0800

    Commit sub project air-china-oauth-common for air-china-oauth.

---
+++ air-china-oauth/air-china-oauth-common/src/main/java/com/stevejrong/airchina/oauth/common/model/BaseModel.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2018 Steve Jrong - https://www.stevejrong.top
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stevejrong.airchina.oauth.common.model;
+
+import java.util.Date;
+
+import com.stevejrong.airchina.common.util.ReflectionUtil;
+
+/**
+ * Model - 基类
+ * 
+ * @author Steve Jrong
+ * @since 1.0 create date: 2018年2月25日 下午11:51:37
+ */
+public class BaseModel {
+	protected Integer id; // 主键
+
+	protected Date gmtCreate; // 创建时间
+
+	protected Date gmtModified; // 修改时间
+
+	protected Integer getId() {
+		return id;
+	}
+
+	protected void setId(Integer id) {
+		this.id = id;
+	}
+
+	protected Date getGmtCreate() {
+		return gmtCreate;
+	}
+
+	protected void setGmtCreate(Date gmtCreate) {
+		this.gmtCreate = gmtCreate;
+	}
+
+	protected Date getGmtModified() {
+		return gmtModified;
+	}
+
+	protected void setGmtModified(Date gmtModified) {
+		this.gmtModified = gmtModified;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionUtil.getAllPropertiesForClassToJsonString(this);
+	}
+}
---
+++ air-china-oauth/air-china-oauth-common/src/main/java/com/stevejrong/airchina/oauth/common/repository/BaseRepository.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2018 Steve Jrong - https://www.stevejrong.top
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stevejrong.airchina.oauth.common.repository;
+
+import java.util.List;
+
+/**
+ * Repository - 基类
+ * 
+ * @author Steve Jrong
+ * @since 1.0 create date: 2018年2月26日 下午12:43:16
+ */
+public interface BaseRepository<T> {
+
+	/**
+	 * 获取单个对象
+	 * 
+	 * @return
+	 */
+	T getOne();
+
+	/**
+	 * 获取对象集合
+	 * 
+	 * @return List对象集合
+	 */
+	List<T> listAll();
+
+	/**
+	 * 获取数据总数
+	 * 
+	 * @return Long类型的数据总数
+	 */
+	Long countAll();
+
+	/**
+	 * 保存单个对象
+	 */
+	void save(T object);
+
+	/**
+	 * 更新单个对象
+	 * 
+	 * @param object
+	 */
+	void update(T object);
+
+	/**
+	 * 移除单个对象
+	 * 
+	 * @param object
+	 */
+	void remove(T object);
+}
